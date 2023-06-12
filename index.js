d89142e0954fdfea98587eae03ada313d331cdae
Hisham6667 <hisham.44678@gmail.com>
2023-06-12 20:48:20 +0600

    delete api for user

--- index.js
+++ index.js
@@ -68,6 +68,12 @@ async function run() {
       const result = await userCollection.find().toArray();
       res.send(result);
     })
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
 
     // jwt token api
     app.post('/jwt', (req, res) => {
